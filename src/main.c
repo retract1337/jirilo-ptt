01e8c5120d7f1d28cb16394f61d6a7c00c96a5ea
Retract <rainfall@hitler.rocks>
2024-12-19 16:16:43 +0100

    Add args

--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <time.h>
-#include <stdlib.h>
+#include <string.h>
 
 #include "include/config.h"
 #include "include/logger.h"
@@ -25,10 +25,6 @@ static struct timespec current_time;
 static struct timespec last_signal_time;
 
 static void on_handler(void) {
-    if (mic_state == MIC_ON) {
-        return;
-    }
-
     set_state(MIC_ON, &backend);
     mic_state = MIC_ON;
 
@@ -37,10 +33,6 @@ static void on_handler(void) {
 }
 
 static void off_handler(void) {
-    if (mic_state == MIC_OFF) {
-        return;
-    }
-
     set_state(MIC_OFF, &backend);
     mic_state = MIC_OFF;
 
@@ -69,15 +61,15 @@ static void ptt_handler(void) {
     LOG_INFO("[%s] PTT pressed", NAME);
 }
 
-void timeout_handler() {
+void timeout_handler(int timeout) {
     clock_gettime(CLOCK_MONOTONIC, &current_time);
 
     long elapsed_sec = current_time.tv_sec - last_signal_time.tv_sec;
     long elapsed_nsec = current_time.tv_nsec - last_signal_time.tv_nsec;
     long elapsed_ms = elapsed_sec * 1000 + elapsed_nsec / 1000000L;
 
-    if (elapsed_ms >= SIGNAL_TIMEOUT && mic_state) {
-        LOG_INFO("[%s] No signal detected for %d ms, disabling microphone...\n", NAME, SIGNAL_TIMEOUT);
+    if (elapsed_ms >= timeout && mic_state) {
+        LOG_INFO("[%s] No signal detected for %d ms, disabling microphone...\n", NAME, timeout);
 
         set_state(MIC_OFF, &backend);
         mic_state = MIC_OFF;
@@ -98,22 +90,24 @@ static CommandMap command_map[] = {
 };
 
 static void process_command(const char *input) {
-    char *token = strtok(input, "\n");
-    while (token != NULL) {
+    char *token = (char *)input; 
+    char *strtok_result = strtok(token, "\n");
+
+    while (strtok_result != NULL) {
         int i;
 
         for (i = 0; command_map[i].command != NULL; i++) {
-            if (strcmp(token, command_map[i].command) == 0) {
+            if (strcmp(strtok_result, command_map[i].command) == 0) {
                 command_map[i].handler();
                 break;
             }
         }
 
         if (command_map[i].command == NULL) {
-            LOG_ERROR("[%s] Unknown command: %s", NAME, token);
+            LOG_ERROR("[%s] Unknown command: %s", NAME, strtok_result);
         }
 
-        token = strtok(NULL, "\n");
+        strtok_result = strtok(NULL, "\n");
     }
 }
 
@@ -124,10 +118,30 @@ void cleanup() {
 }
 
 int main(int argc, char *argv[]) {
+    int opt;
+
+    int custom_check_interval = DEFAULT_CHECK_INTERVAL;
+    int custom_timeout = SIGNAL_TIMEOUT;
+
     atexit(cleanup); 
     unlink(FIFO_PATH);  
-
     detect_backend(&backend);
+
+    while ((opt = getopt(argc, argv, "t:")) != -1) {
+        switch (opt) {
+            case 't':
+                custom_timeout = atoi(optarg);
+                break;
+            case 'i':
+                custom_check_interval = atoi(optarg);
+                break;
+            default:
+                LOG_ERROR("[%s] Unknown option: %c", NAME, opt);
+                LOG_INFO("[%s] Usage: %s [-t timeout] [-i interval]", NAME, argv[0]);
+
+                exit(EXIT_FAILURE);
+        }
+    }
 
     if (mkfifo(FIFO_PATH, 0666) == -1) {
         perror("Failed to create FIFO");
@@ -152,8 +166,8 @@ int main(int argc, char *argv[]) {
             process_command(buffer);
         }
 
-        timeout_handler();
-        usleep(DEFAULT_CHECK_INTERVAL * 1000);
+        timeout_handler(custom_timeout);
+        usleep(custom_check_interval * 1000);
     }
 
     close(fd);
